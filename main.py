d9a86ac675b8c330be652663884a4dc4193ed1c5
Milena <milena.markovic.88@gmail.com>
2025-03-15 16:09:00 +0100

    Dodata klasa Cart za upravljanje korpom

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from product_manager import ProductManager
 from product import Product
+from cart import Cart
+
 
 manager = ProductManager()
 
-# Dodavanje proizvoda
 p1 = Product("Telefon", 50000, 10)
 p2 = Product("Laptop", 120000, 5)
 p3 = Product("Slusalice", 3000, 15)
@@ -12,6 +13,18 @@ manager.add_product(p1)
 manager.add_product(p2)
 manager.add_product(p3)
 
-# Prikaz svih proizvoda i ukupne vrednosti
+
+
+cart = Cart()
+
+cart.add_to_cart(p1)
+cart.add_to_cart(p2)
+cart.add_to_cart(p3)
+
+
 manager.display_products()
 print(f"Ukupna vrednost inventara: {manager.total_inventory_value()} RSD")
+
+print("Sadržaj korpe:")
+cart.display_cart()
+print(f"Ukupna vrednost korpe: {cart.total_cart_value()} RSD")
